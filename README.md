c6bc4823bf721b2e4707f2c96ac5272b3c7749d9
mjunaid249 <junaidch249249@gmail.com>
2024-02-22 08:39:05 +0500

    Todo-React-Js

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Todo App (React Js Project)
+
+This is a Simple Todo app developed to Understand the Basic Concept of UseState Hook in React
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">iTask</div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Tasks</li>
+          <li>About</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
